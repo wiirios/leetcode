a546b56c03b284c706228defd788135a86a71597
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-09-24 09:13:29 -0300

    Add P1114 Solution and P1115 Solution

---
+++ Java/1114-Print-In-Order/problem.java
@@ -0,0 +1,27 @@
+class Foo {
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        Thread.sleep(100);
+        printFirst.run();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        Thread.sleep(200);
+        printSecond.run();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
+        // printThird.run() outputs "third". Do not change or remove this line.
+        Thread.sleep(300);
+        printThird.run();
+    }
+}
---
+++ Java/1115-Print-FooBar-Alternately/problem.java
@@ -0,0 +1,27 @@
+class FooBar {
+    private int n;
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+
+        	// printFoo.run() outputs "foo". Do not change or remove this line.
+            Thread.sleep(100);
+        	printFoo.run();
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+        
+        for (int i = 0; i < n; i++) {
+            
+            // printBar.run() outputs "bar". Do not change or remove this line.
+            Thread.sleep(200);
+        	printBar.run();
+        }
+    }
+}
